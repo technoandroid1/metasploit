ae1c7b5debfd16c5a58add56cd17b81dcc597c8a
technoandroid1 <34939136+technoandroid1@users.noreply.github.com>
2018-01-07 12:04:55 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -1,76 +1,65 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo "##############################################"
-echo " I  AM  NISHANT , PLZ SUBSCRIBE MY CHANNEL  TECHNO ANDROID"
+echo " TechZindia, PLZ SUBSCRIBE TechZindia YouTube CHANNEL"
 echo "##############################################"
 
 echo "WAIT UNTIL INSTALLING............" 
 
 echo "####################################"
-apt install -y autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config postgresql-contrib wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
+apt install autoconf bison clang coreutils curl findutils git apr figlet apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config postgresql-contrib wget make ruby-dev libgrpc-dev ncurses-utils termux-tools -y
 echo "####################################"
-apt update && apt upgrade
 echo "Downloading & Extracting....."
 
 cd $HOME
-curl -LO https://github.com/rapid7/metasploit-framework/archive/4.16.16.tar.gz
-tar -xf $HOME/4.16.16.tar.gz
-mv $HOME/metasploit-framework-4.16.16 $HOME/metasploit-framework
-rm $HOME/4.16.16.tar.gz
+curl -LO https://github.com/rapid7/metasploit-framework/archive/4.16.4.tar.gz
+tar -xf $HOME/4.16.4.tar.gz
+mv $HOME/metasploit-framework-4.16.4 $HOME/metasploit-framework
 cd $HOME/metasploit-framework
 sed '/rbnacl/d' -i Gemfile.lock
 sed '/rbnacl/d' -i metasploit-framework.gemspec
+
+#Install bundler
 gem install bundler
-sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
 
-gem install nokogiri -v 1.8.0 -- --use-system-libraries
- 
-#sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock
-#gem unpack grpc -v 1.4.1
-#cd grpc-1.4.1
-#curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
-#curl -L https://raw.githubusercontent.com/Hax4us/Hax4us.github.io/master/extconf.patch
-#patch -p1 < extconf.patch
-#gem build grpc.gemspec
-#gem install grpc-1.4.1.gem
-#cd ..
-#rm -r grpc-1.4.1
+#Install nokogiri
+echo "nokogiri is installing......"
+gem install nokogiri -- --use-system-libraries
 
+#Install grpc gem
+sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock
+gem unpack grpc -v 1.4.1
+cd grpc-1.4.1
+curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
+curl -L https://wiki.termux.com/images/b/bf/Grpc_extconf.patch -o extconf.patch
+patch -p1 < extconf.patch
+gem build grpc.gemspec
+gem install grpc-1.4.1.gem
+cd ..
+rm -r grpc-1.4.1
 
+#Bundle Install
+echo "bundle and all other dependencies are installing......"
 cd $HOME/metasploit-framework
 bundle install -j5
+
+#Fixing Shebang
 $PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
 rm ./modules/auxiliary/gather/http_pdf_authors.rb
-if [ -e $PREFIX/bin/msfconsole ];then
-	rm $PREFIX/bin/msfconsole
-fi
-if [ -e $PREFIX/bin/msfvenom ];then
-	rm $PREFIX/bin/msfvenom
-fi
 ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
-ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
-termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
-echo "Creating database"
-
-cd $HOME/metasploit-framework/config
-curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/database.yml
-
-mkdir -p $PREFIX/var/lib/postgresql
-initdb $PREFIX/var/lib/postgresql
-
-pg_ctl -D $PREFIX/var/lib/postgresql start
-createuser msf
-createdb msf_database
 
 echo "###############################"
-echo "Thanx  To  Vishalbiswani"
+echo "Thanks  To  Vishalbiswani & Auxilus "
 echo "###############################"
-
+figlet TechZindia
 echo "###############################################"
-echo "Hello  I  Am  Nishant , Subscribe  My  Channel techno android To  Motivate  My  Work"
+echo "Hello Guys,  Subscribe  My TechZindia YouTube channel"
+figlet Subscribe
 echo "###############################################"
 echo "###############################"
-echo "ANY PROBLEM CONTACT ME ON WHATSAPP (8999803586)"
+echo "For More Hacking video At TechZindia YT Channel"
 echo "###############################"
+figlet TechZindia
 echo "####################################"
-echo " NOW YOU CAN LAUNCH METASPLOIT BY JUST EXECUTE THE COMMAND :=> msfconsole"
+echo " NOW YOU CAN RUN METASPLOIT BY JUST EXECUTE THE COMMAND :=>  ./msfconsole"
 echo "####################################"
+figlet Finish
